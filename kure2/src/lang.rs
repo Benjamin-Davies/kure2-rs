d839e4497f2aceb9cb95735dd0d496f3dcb66e36
Benjamin Davies <bentendavies@gmail.com>
2025-08-11 21:30:11 +1200

    feat: more parsing methods

--- kure2/src/lang.rs
+++ kure2/src/lang.rs
@@ -1,11 +1,29 @@
 //! The Kure-Lua bindings and the embedded language.
 
-use std::{ffi::CString, ptr};
+use std::{
+    ffi::{CStr, CString, c_void},
+    ptr,
+};
 
 use kure2_lua_sys as lua_ffi;
 use kure2_sys as ffi;
 
 use crate::{Context, Error, Relation, context};
+
+/// Callbacks for [`parse`] and [`parse_file`].
+pub trait ParserObserver {
+    /// Called with the original code as well as the generated Lua code whenever a function is parsed.
+    fn on_function(&mut self, original_code: &str, lua_code: &str) -> bool {
+        let _ = (original_code, lua_code);
+        true
+    }
+
+    /// Called with the original code as well as the generated Lua code whenever a program (procedure) is parsed.
+    fn on_program(&mut self, original_code: &str, lua_code: &str) -> bool {
+        let _ = (original_code, lua_code);
+        true
+    }
+}
 
 /// State for the Kure-Lua bindings and the embedded language.
 pub struct LuaState {
@@ -98,7 +116,7 @@ impl LuaState {
 
     /// Loads the contents of the given file using [`LuaState::load`].
     pub fn load_file(&mut self, file: &str) -> Result<(), Error> {
-        let c_file = CString::new(file).expect("file contains null byte");
+        let c_file = CString::new(file).expect("file name contains null byte");
 
         let mut error_ptr = ptr::null_mut();
         let success =
@@ -125,6 +143,24 @@ impl Drop for LuaState {
     }
 }
 
+/// Converts a translation unit (source file) in the embedded language to a Lua expression.
+pub fn to_lua(transl_unit: &str) -> Result<String, Error> {
+    let c_transl_unit = CString::new(transl_unit).expect("transl_unit contains null byte");
+
+    let mut error_ptr = ptr::null_mut();
+    let lua_str_ptr = unsafe { ffi::kure_lang_to_lua(c_transl_unit.as_ptr(), &mut error_ptr) };
+    if lua_str_ptr.is_null() {
+        let error = unsafe { Error::from_ffi(error_ptr) };
+        unsafe { ffi::kure_error_destroy(error_ptr) };
+        return Err(error);
+    }
+
+    let lua_str = unsafe { CString::from_raw(lua_str_ptr) };
+    Ok(lua_str
+        .into_string()
+        .expect("lua string contains invalid UTF-8"))
+}
+
 /// Converts an expression in the embedded language to a Lua expression.
 pub fn expr_to_lua(expr: &str) -> Result<String, Error> {
     let c_expr = CString::new(expr).expect("expr contains null byte");
@@ -143,9 +179,91 @@ pub fn expr_to_lua(expr: &str) -> Result<String, Error> {
         .expect("lua string contains invalid UTF-8"))
 }
 
+/// Parses the given translation unit. Each time a function or program is parsed the corresponding
+/// callback function (if non-NULL) is called and provided the original code as well as the
+/// generated Lua code.
+pub fn parse(transl_unit: &str, observer: &mut impl ParserObserver) -> Result<(), Error> {
+    let c_transl_unit = CString::new(transl_unit).expect("transl_unit contains null byte");
+    let mut ffi_observer = make_ffi_observer(observer);
+
+    let mut error_ptr = ptr::null_mut();
+    let success =
+        unsafe { ffi::kure_lang_parse(c_transl_unit.as_ptr(), &mut ffi_observer, &mut error_ptr) };
+    if success == 0 {
+        let error = unsafe { Error::from_ffi(error_ptr) };
+        unsafe { ffi::kure_error_destroy(error_ptr) };
+        return Err(error);
+    }
+
+    Ok(())
+}
+
+/// Parses the contents of the given file using [`parse`].
+pub fn parse_file(file: &str, observer: &mut impl ParserObserver) -> Result<(), Error> {
+    let c_file = CString::new(file).expect("file name contains null byte");
+    let mut ffi_observer = make_ffi_observer(observer);
+
+    let mut error_ptr = ptr::null_mut();
+    let success =
+        unsafe { ffi::kure_lang_parse_file(c_file.as_ptr(), &mut ffi_observer, &mut error_ptr) };
+    if success == 0 {
+        let error = unsafe { Error::from_ffi(error_ptr) };
+        unsafe { ffi::kure_error_destroy(error_ptr) };
+        return Err(error);
+    }
+
+    Ok(())
+}
+
+fn make_ffi_observer<O: ParserObserver>(observer: &mut O) -> ffi::KureParserObserver {
+    type StateObject<O> = *mut O;
+
+    unsafe extern "C" fn on_function<O: ParserObserver>(
+        object: *mut c_void,
+        c_original_code: *const i8,
+        c_lua_code: *const i8,
+    ) -> i8 {
+        let observer = unsafe { &mut *(object as StateObject<O>) };
+        let original_code = unsafe { CStr::from_ptr(c_original_code) }
+            .to_str()
+            .expect("original_code is not valid UTF-8");
+        let lua_code = unsafe { CStr::from_ptr(c_lua_code) }
+            .to_str()
+            .expect("lua_code is not valid UTF-8");
+
+        let result = observer.on_function(original_code, lua_code);
+
+        result as i8
+    }
+
+    unsafe extern "C" fn on_program<O: ParserObserver>(
+        object: *mut c_void,
+        c_original_code: *const i8,
+        c_lua_code: *const i8,
+    ) -> i8 {
+        let observer = unsafe { &mut *(object as StateObject<O>) };
+        let original_code = unsafe { CStr::from_ptr(c_original_code) }
+            .to_str()
+            .expect("original_code is not valid UTF-8");
+        let lua_code = unsafe { CStr::from_ptr(c_lua_code) }
+            .to_str()
+            .expect("lua_code is not valid UTF-8");
+
+        let result = observer.on_program(original_code, lua_code);
+
+        result as i8
+    }
+
+    ffi::KureParserObserver {
+        onFunction: Some(on_function::<O>),
+        onProgram: Some(on_program::<O>),
+        object: observer as StateObject<O> as *mut c_void,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::lang::{self, LuaState};
+    use crate::lang::{self, LuaState, ParserObserver};
 
     #[test]
     fn test_create_destroy_lua_state() {
@@ -195,11 +313,70 @@ mod tests {
     }
 
     #[test]
+    fn test_to_lua() {
+        let prog = include_str!("../../kure2-sys/kure2-2.2/data/programs/Aux.prog");
+
+        let result = lang::to_lua(prog).unwrap();
+
+        assert!(result.starts_with("function rc"));
+    }
+
+    #[test]
     fn test_expr_to_lua() {
         let expr = "R|S";
 
         let result = lang::expr_to_lua(expr).unwrap();
 
         assert_eq!(result, "kure.lor(R,S)");
+    }
+
+    #[derive(Default, Debug)]
+    struct TestObserver {
+        functions: Vec<(String, String)>,
+        programs: Vec<(String, String)>,
+    }
+
+    impl ParserObserver for TestObserver {
+        fn on_function(&mut self, original_code: &str, lua_code: &str) -> bool {
+            self.functions
+                .push((original_code.to_string(), lua_code.to_string()));
+            true
+        }
+
+        fn on_program(&mut self, original_code: &str, lua_code: &str) -> bool {
+            self.programs
+                .push((original_code.to_string(), lua_code.to_string()));
+            true
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let prog = include_str!("../../kure2-sys/kure2-2.2/data/programs/Aux.prog");
+        let mut observer = TestObserver::default();
+
+        lang::parse(prog, &mut observer).unwrap();
+
+        assert_eq!(observer.functions.len(), 81);
+        assert_eq!(observer.functions[0].0, "rc(R)  = refl(R).");
+        assert!(observer.functions[0].1.starts_with("function rc"));
+        assert_eq!(observer.programs.len(), 3);
+        assert!(observer.programs[0].0.starts_with("randompoint(v)"));
+        assert!(observer.programs[0].1.starts_with("function randompoint"));
+    }
+
+    #[test]
+    fn test_parse_file() {
+        let file = "../kure2-sys/kure2-2.2/data/programs/Aux.prog";
+        let mut observer = TestObserver::default();
+
+        lang::parse_file(file, &mut observer).unwrap();
+
+        assert_eq!(observer.functions.len(), 81);
+        assert_eq!(observer.functions[0].0, "rc(R)  = refl(R).");
+        assert!(observer.functions[0].1.starts_with("function rc"));
+        assert_eq!(observer.programs.len(), 3);
+        assert!(observer.programs[0].0.starts_with("randompoint(v)"));
+        assert!(observer.programs[0].1.starts_with("function randompoint"));
     }
 }
